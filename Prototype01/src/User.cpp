beb66b3ebe017053cd4ccb22da3b9920284b4b6b
Jeff Crouse <jefftimesten@gmail.com>
2013-08-12 13:06:42 -0400

    first commit

---
+++ Prototype01/src/User.cpp
@@ -0,0 +1,15 @@
+//
+//  User.cpp
+//  Prototype01
+//
+//  Created by Jeffrey Crouse on 8/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include "User.h"
+
+float User::radius_min = 100;
+float User::radius_max = 500;
+float User::age_max = 60*5;
+float User::multiplier_min = 30;
+float User::multiplier_max = 50;
