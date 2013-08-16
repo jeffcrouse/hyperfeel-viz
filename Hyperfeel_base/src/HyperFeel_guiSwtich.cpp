cf39bf5cb1c59cc3cada347459362dbdadc844ca
lars berg <laserstorm@gmail.com>
2013-08-15 23:39:16 -0500

    moved over to ofxUI

---
+++ Hyperfeel_base/src/HyperFeel_guiSwtich.cpp
@@ -0,0 +1,9 @@
+//
+//  HyperFeel_guiSwtich.cpp
+//  Hyperfeel_base
+//
+//  Created by laserstorm on 8/14/13.
+//
+//
+
+#include "HyperFeel_guiSwtich.h"
---
+++ Hyperfeel_base/src/HyperFeel_guiSwtich.h
@@ -0,0 +1,14 @@
+//
+//  HyperFeel_guiSwtich.h
+//  Hyperfeel_base
+//
+//  Created by laserstorm on 8/14/13.
+//
+//
+
+#ifndef Hyperfeel_base_HyperFeel_guiSwtich_h
+#define Hyperfeel_base_HyperFeel_guiSwtich_h
+
+
+
+#endif
